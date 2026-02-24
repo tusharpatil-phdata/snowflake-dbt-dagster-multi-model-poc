e6273971a5413991fc5ba38ff6414d12722df56d
tusharpatil-phdata <tpatil@phdata.io>
2026-02-24 07:25:50 +0530

    Update mart_product_performance.sql

--- models/marts/mart_product_performance.sql
+++ models/marts/mart_product_performance.sql
@@ -1,19 +1,32 @@
+{{ config(schema='MART') }}
+
 {{ config(materialized='table') }}
 
 WITH order_details AS (
     SELECT * FROM {{ ref('int_order_details') }}
 ),
+
 product_metrics AS (
     SELECT
         part_key,
         part_name,
         manufacturer,
         brand,
-        COUNT(DISTINCT order_key)     AS times_ordered,
-        SUM(quantity)                 AS total_quantity_sold,
-        SUM(net_price)                AS total_revenue,
-        AVG(net_price / NULLIF(quantity,0)) AS avg_unit_price
+        COUNT(DISTINCT order_key) AS times_ordered,
+        SUM(quantity) AS total_quantity_sold,
+        SUM(net_price) AS total_revenue,
+        AVG(net_price / quantity) AS avg_unit_price,
+        SUM(extended_price * discount) AS total_discount_given,
+        -- Performance indicators
+        ROUND(SUM(net_price) / NULLIF(SUM(quantity), 0), 2) AS revenue_per_unit,
+        ROUND(SUM(extended_price * discount) / NULLIF(SUM(extended_price), 0) * 100, 2) AS avg_discount_pct
     FROM order_details
-    GROUP BY part_key, part_name, manufacturer, brand
+    GROUP BY 
+        part_key,
+        part_name,
+        manufacturer,
+        brand
 )
+
 SELECT * FROM product_metrics
+ORDER BY total_revenue DESC
