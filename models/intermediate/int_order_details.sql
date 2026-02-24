796e3b3275e57ce987ceb2c145fc46df2b613424
tusharpatil-phdata <tpatil@phdata.io>
2026-02-24 07:22:41 +0530

    Update int_order_details.sql

--- models/intermediate/int_order_details.sql
+++ models/intermediate/int_order_details.sql
@@ -1,14 +1,19 @@
+{{ config(schema='INTERMEDIATE') }}
+
 {{ config(materialized='table') }}
 
 WITH orders AS (
     SELECT * FROM {{ ref('stg_tpch_orders') }}
 ),
+
 lineitems AS (
     SELECT * FROM {{ ref('stg_tpch_lineitems') }}
 ),
+
 parts AS (
     SELECT * FROM {{ ref('stg_tpch_parts') }}
 ),
+
 joined AS (
     SELECT
         o.order_key,
@@ -24,14 +29,15 @@ joined AS (
         l.extended_price,
         l.discount,
         l.tax,
-        -- net price after discount
+        -- Business logic: calculate net price after discount
         l.extended_price * (1 - l.discount) AS net_price,
-        -- total with tax
+        -- Business logic: calculate total with tax
         l.extended_price * (1 - l.discount) * (1 + l.tax) AS total_price_with_tax,
         l.ship_date,
         l.ship_mode
     FROM orders o
-    JOIN lineitems l ON o.order_key = l.order_key
-    JOIN parts p ON l.part_key = p.part_key
+    INNER JOIN lineitems l ON o.order_key = l.order_key
+    INNER JOIN parts p ON l.part_key = p.part_key
 )
+
 SELECT * FROM joined
