595e4b6987bfe9a929e9badb1f4b368b02ecc40a
tusharpatil-phdata <tpatil@phdata.io>
2026-02-24 07:21:47 +0530

    Update int_customer_orders.sql

--- models/intermediate/int_customer_orders.sql
+++ models/intermediate/int_customer_orders.sql
@@ -1,11 +1,15 @@
+{{ config(schema='INTERMEDIATE') }}
+
 {{ config(materialized='table') }}
 
 WITH customers AS (
     SELECT * FROM {{ ref('stg_tpch_customers') }}
 ),
+
 orders AS (
     SELECT * FROM {{ ref('stg_tpch_orders') }}
 ),
+
 joined AS (
     SELECT
         c.customer_key,
@@ -17,15 +21,17 @@ joined AS (
         o.order_status,
         o.total_price,
         o.order_priority,
-        -- example: order size buckets
+        -- Business logic: categorize order size
         CASE
             WHEN o.total_price < 50000 THEN 'Small'
             WHEN o.total_price < 150000 THEN 'Medium'
             WHEN o.total_price < 300000 THEN 'Large'
             ELSE 'Enterprise'
         END AS order_size_category,
+        -- Business logic: order age in days
         DATEDIFF(day, o.order_date, CURRENT_DATE()) AS order_age_days
     FROM customers c
-    JOIN orders o ON c.customer_key = o.customer_key
+    INNER JOIN orders o ON c.customer_key = o.customer_key
 )
+
 SELECT * FROM joined
