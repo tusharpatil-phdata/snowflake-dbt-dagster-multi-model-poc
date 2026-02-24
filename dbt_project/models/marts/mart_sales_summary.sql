02f6ea69dd3fc534a0aad18887f9b605da5cb1c4
tusharpatil-phdata <tpatil@phdata.io>
2026-02-24 14:14:29 +0530

    set loaded_at as UTC+5h30m for IST

--- dbt_project/models/marts/mart_sales_summary.sql
+++ dbt_project/models/marts/mart_sales_summary.sql
@@ -24,8 +24,9 @@ daily_sales AS (
         SUM(net_price)                  AS total_revenue,
         SUM(extended_price * discount)  AS total_discounts,
         AVG(net_price)                  AS avg_order_line_value,
-        -- Load timestamp in IST
-        CONVERT_TIMEZONE('UTC', 'Asia/Kolkata', CURRENT_TIMESTAMP())::timestamp_tz AS loaded_at
+        
+        -- Load timestamp approximating IST (UTC + 5h30m)
+        DATEADD('minute', 330, CURRENT_TIMESTAMP())::timestamp_ntz AS loaded_at
     FROM order_details
     GROUP BY order_date
 )
